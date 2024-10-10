d945f949b933b181f96d817cfad6448237ad5039
20221tiimi0190 <kalebe.olegario1850@gmail.com>
2024-10-10 12:34:52 -0300

    a

--- exercicios3/1.php
+++ exercicios3/1.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Relatorio de Cliente</title>
+    <link rel="stylesheet" href="css_exer3.css">
 </head>
 <body>
     <h1>Relatório de cliente</h1>
